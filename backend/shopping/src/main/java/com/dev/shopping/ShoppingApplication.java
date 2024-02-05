e87815f6d3810dc47e7399dc2cb5bed5f426fdcf
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-02-05 15:32:28 +0900

    init

--- backend/shopping/src/main/java/com/dev/shopping/ShoppingApplication.java
+++ backend/shopping/src/main/java/com/dev/shopping/ShoppingApplication.java
@@ -1,13 +1,13 @@
-package com.stock.trading;
+package com.dev.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TradingApplication {
+public class ShoppingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TradingApplication.class, args);
+		SpringApplication.run(ShoppingApplication.class, args);
 	}
 
 }
