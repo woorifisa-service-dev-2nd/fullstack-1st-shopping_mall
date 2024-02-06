90775f65ce3c900030f211445b5e1d5c535d87c3
Kyongryul Na <skrudfbf0813@naver.com>
2024-02-06 10:06:39 +0900

    orderPage front

---
+++ front/shopping/src/pages/order/Order.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+export default function Order() {
+
+    const location = useLocation();
+    const orderList = location.state ? location.state.orderList : [];
+    console.log(orderList);
+    return (
+        <div>
+            <h2>주문 목록</h2>
+            <ul>
+                {orderList.map(product => (
+                    <li key={product.id}>
+                        <span>ID: {product.id}</span>
+                        <span>Name: {product.name}</span>
+                        <span>Price: {product.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
