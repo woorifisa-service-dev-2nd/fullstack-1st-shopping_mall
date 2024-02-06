12a6a2fd0c1cbf916c23070637ee7b9ce3dd5db7
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2024-02-06 12:22:45 +0900

    fix: mainPage design

--- front/shopping/src/pages/order/Order.jsx
+++ front/shopping/src/pages/order/Order.jsx
@@ -1,23 +1,24 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from "react-router-dom";
+import DefaultLayout from "../../layouts/DefaultLayouts";
 
 export default function Order() {
-
-    const location = useLocation();
-    const orderList = location.state ? location.state.orderList : [];
-    console.log(orderList);
-    return (
-        <div>
-            <h2>주문 목록</h2>
-            <ul>
-                {orderList.map(product => (
-                    <li key={product.id}>
-                        <span>ID: {product.id}</span>
-                        <span>Name: {product.name}</span>
-                        <span>Price: {product.price}</span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const location = useLocation();
+  const orderList = location.state ? location.state.orderList : [];
+  console.log(orderList);
+  return (
+    <DefaultLayout>
+      <div>
+        <h2>주문 목록</h2>
+        <ul>
+          {orderList.map((product) => (
+            <li key={product.id}>
+              <span>ID: {product.id}</span>
+              <span>Name: {product.name}</span>
+              <span>Price: {product.price}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </DefaultLayout>
+  );
 }
-
